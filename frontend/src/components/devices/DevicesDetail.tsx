11f2aeb92fc9e7286c2b01167140efaaa0eb7004
harrypham <pham_quanghuy@wohhup.com.vn>
2024-12-16 19:24:30 +0700

    creae form

--- frontend/src/components/devices/DevicesDetail.tsx
+++ frontend/src/components/devices/DevicesDetail.tsx
@@ -1,12 +1,10 @@
 import React, { useMemo, useState } from 'react';
-import DevicesTopBar from './DevicesTopBar';
-import { Button, Tooltip } from 'antd';
-import { DeleteOutlined, EditOutlined, EyeOutlined, RollbackOutlined } from '@ant-design/icons';
+import { Button, Form, Tooltip } from 'antd';
 import { useNavigate } from 'react-router-dom';
 
 const DevicesDetail = () => {
     let navigate = useNavigate();
-    
+
     return (
         <div className="medi-devices-detail">
             <div className="devices-detail__topbar">
@@ -14,13 +12,27 @@ const DevicesDetail = () => {
                     <div className='text-xl	font-bold'>Lý lịch thiết bị</div>
                 </div>
                 <div className='detail__topbar--right'>
-                    <Button variant="dashed" style={{marginRight: '8px'}} onClick={() => navigate('/devices')}>Hủy bỏ</Button>
-                    <Button className='btn-main-2' style={{marginRight: '8px'}} >Lưu và thêm</Button>
+                    <Button variant="dashed" className='btn-main-3' style={{ marginRight: '8px' }} onClick={() => navigate('/devices')}>Hủy bỏ</Button>
+                    <Button className='btn-main-2' style={{ marginRight: '8px' }} >Lưu và thêm</Button>
                     <Button className='btn-main'>Lưu</Button>
                 </div>
             </div>
             <div className='devices-detail__content'>
-               
+                <Form
+                    name="basic"
+                    style={{ width: '100%', height: '100%' }}
+                    initialValues={{ remember: true }}
+                    onFinish={() => {}}
+                    onFinishFailed={() => {}}
+                    autoComplete="off"
+                >
+                    <div className='detail__content--left'>
+
+                    </div>
+                    <div className='detail__content--right'>
+
+                    </div>
+                </Form>
             </div>
         </div>
     );
